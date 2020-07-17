68aa62a91f30064d4511ac929fbb36f0239a69d6
Chiara <ada.chiara.capuano@gmail.com>
2020-07-17 09:20:17 -0700

    Encoding outside pipeline

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 import numpy as np
-from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
@@ -19,9 +19,9 @@ from sklearn.model_selection import GridSearchCV
 
 pd.set_option("display.max_rows", 2000)
 train_i = pd.read_csv('/Users/chiara/PycharmProjects/KaggleHousePrices/house-prices-advanced-regression-techniques/train.csv')
-test = pd.read_csv('/Users/chiara/PycharmProjects/KaggleHousePrices/house-prices-advanced-regression-techniques/test.csv')
+test_submission = pd.read_csv('/Users/chiara/PycharmProjects/KaggleHousePrices/house-prices-advanced-regression-techniques/test.csv')
 
-
+#train_i = pd.concat([train_init, test_submission])
 train_i['MSSubClass'] = train_i['MSSubClass'].astype(str)
 
 
@@ -39,7 +39,6 @@ categorical_cols = []
 for col in train_i.columns:
     if train_i[col].dtype == 'object':
         categorical_cols.append(col)
-        print(col)
     else:
         numerical_cols.append(col)
 
@@ -63,8 +62,10 @@ le = LabelEncoder()
 for col in categorical_cols:
     trial_df_le[col] = le.fit_transform(train_i[col].astype(str))
 
-
 trial_df_le['SalePrice'] = train_i['SalePrice']
+
+
+
 dfm_le = trial_df_le[[col for col in trial_df_le if trial_df_le[col].nunique() > 1]] # keep columns where there are more than 1 unique values
 print('2')
 
@@ -104,7 +105,6 @@ cat_le = corr_le_keep.index.tolist()
 
 cat_le.remove('SalePrice')
 cat_ohe = list((Counter(categorical_cols)-Counter(cat_le)).elements())
-print(cat_le,cat_ohe)
 
 
 for col in cat_le:
@@ -113,13 +113,22 @@ for col in cat_le:
 train_processed = pd.get_dummies(train_i,
                               columns=cat_ohe)
 
-n_cat_ohe=[]
-for col in train_processed:
-    if col not in train_i.columns:
-        n_cat_ohe.append(col)
+
+
+dummy_submission = pd.get_dummies(test_submission,
+                              columns=cat_ohe)
+
+
+for col in cat_le:
+    dummy_submission[col] = le.fit_transform(test_submission[col].astype(str))
+
+#dummy_submission.drop(columns = cat_le, inplace=True)
+
+
+dummy_submission=dummy_submission.reindex(columns = train_processed.columns, fill_value=0)
 
 train_inter, test_inter = train_test_split(
-    train_processed, test_size=0.1)
+    train_processed, test_size=0.2)
 
 train, test = train_inter.align(test_inter, join='outer', axis=1)
 X_train = train.drop(columns=['Id', 'SalePrice'])
@@ -127,8 +136,6 @@ X_test = test.drop(columns=['Id', 'SalePrice'])
 
 y_train = train['SalePrice']
 y_test = test['SalePrice']
-
-print(X_train.head())
 
 
 categorical_transformer_ohe = Pipeline(steps=[
@@ -143,8 +150,6 @@ categorical_transformer_le = Pipeline(steps=[
 
 
 print('4')
-print(X_train.columns)
-print(len(X_test.columns))
 n_cat_ohe = X_train.columns
 # Bundle preprocessing for numerical and categorical data
 preprocessor = ColumnTransformer(
@@ -153,41 +158,52 @@ preprocessor = ColumnTransformer(
         #('cat_le', categorical_transformer_le, cat_le),
         ('num', numerical_transformer_NaN, numerical_cols)
 
-    ])
+
+    ], remainder='passthrough')
 
 
 clf = GradientBoostingRegressor()
 # Bundle preprocessing and modeling code in a pipeline
 pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('reduce_dim', SelectKBest(mutual_info_classif)),
                             ('clf', GradientBoostingRegressor(random_state=0))
                              ])
 print('5')
 
-# Preprocessing of training data, fit model
-#pipeline.fit(X_train, y_train)
-# Preprocessing of validation data, get predictions
-#preds = pipeline.predict(X_test)
 
-
-# Evaluate the model
-#score = mean_squared_error(y_test, preds)
-#print('MSE:', score)
-#yhat = sc_Y.inverse_transform(preds.reshape(-1, 1))
-#y_test_transf = sc_Y.inverse_transform(y_test)
-#print(preds, y_test)
-#a = list(zip(preds, y_test))
 parameters = [{
-        'clf__learning_rate': [0.1, 0.05, 0.02, 0.01],
-        'clf__max_depth': [6, 4],
-        'clf__n_estimators': [100, 200]}]
+        'reduce_dim__k': [100, 200, 278],
+        'clf__max_features': [1.0,0.3,0.1],
+        'clf__min_samples_leaf': [3,5,9,17],
+        'clf__max_depth': [6, 4, 2],
+        'clf__n_estimators': [100, 200, 300]}]
 
 
 
-    #{'n_estimators':[100], 'learning_rate': [0.1,0.05, 0.02, 0.01], 'max_depth':[6,4,6], 'min_samples_leaf':[3,5,9,17], 'max_features':[1.0,0.3,0.1] }#github.io/stories/gbrt-sklearn.html#sthash.xTY0Gw5J.dpuf]
-#parameters['feat_select__k'] = np.arange(5, 135, 5)
-CV = GridSearchCV(pipeline, parameters, n_jobs=1)
+CV = GridSearchCV(pipeline, parameters, scoring = 'explained_variance', n_jobs=1)
 CV.fit(X_train, y_train)
-print(CV.best_score_)
 print(CV.best_params_)
+best_params_dict = CV.best_params_
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('reduce_dim', SelectKBest(mutual_info_classif, k = best_params_dict["reduce_dim__k"])),
+                            ('clf', GradientBoostingRegressor(random_state=0, max_features = best_params_dict["clf__max_features"], min_samples_leaf = best_params_dict["clf__min_samples_leaf"], max_depth = best_params_dict["clf__max_depth"], n_estimators = best_params_dict["clf__n_estimators"]
 
-y_pred = CV.predict(X_test)
+                                                              ))
+                             ])
+
+print('5')
+
+# Preprocessing of training data, fit model
+pipeline.fit(X_train, y_train)
+# Preprocessing of validation data, get predictions
+preds = pipeline.predict(X_test)
+score = pipeline.score(X_test, y_test)
+dummy_submission_to_pred = dummy_submission.drop(columns=['Id', 'SalePrice'])
+
+preds = pipeline.predict(dummy_submission_to_pred)
+print(preds)
+df_submit = pd.DataFrame()
+df_submit["Id"] = dummy_submission["Id"]
+
+df_submit["SalePrice"]= preds
+df_submit.to_csv('/Users/chiara/PycharmProjects/KaggleHousePrices/house-prices-advanced-regression-techniques/Chiara_best_model_submission.csv', index=False)
